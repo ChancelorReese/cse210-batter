2eb50fdaa040b32db18ceaf84c8d86ed9e24a17f
ChancelorReese <Chancelor.Reese@gmail.com>
2021-11-20 16:00:31 -0700

    new classes

---
+++ batter/person.py
@@ -0,0 +1,22 @@
+class Person:
+    __first_name = ""
+    __last_name = ""
+
+def __init__(): 
+    p1 = Person
+    p1.__first_name = "Chancelor"
+    p1.__last_name = "Reese"
+
+    p2 = Person
+    p2.__first_name = "Shaelee"
+    p2.__last_name = "Sommerfeldt"
+
+    p3 = Person
+    p3.__first_name = "Michael"
+    p3.__last_name = "Fisher"
+
+def set_name():
+    pass
+
+def get_name():
+    pass
